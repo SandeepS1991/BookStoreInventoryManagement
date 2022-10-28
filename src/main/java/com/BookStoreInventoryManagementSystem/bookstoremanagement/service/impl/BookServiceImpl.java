83d9fadf40b6787f9fccdc1bda872b79b71aa776
SandeepS1991 <sudasandeep91@gmail.com>
2022-10-28 19:38:20 +0800

    API AddNewBook was not saving author details. Added that functionality.

--- src/main/java/com/BookStoreInventoryManagementSystem/bookstoremanagement/service/impl/BookServiceImpl.java
+++ src/main/java/com/BookStoreInventoryManagementSystem/bookstoremanagement/service/impl/BookServiceImpl.java
@@ -1,6 +1,8 @@
 package com.BookStoreInventoryManagementSystem.bookstoremanagement.service.impl;
 
+import com.BookStoreInventoryManagementSystem.bookstoremanagement.entity.AuthorEntity;
 import com.BookStoreInventoryManagementSystem.bookstoremanagement.entity.BookEntity;
+import com.BookStoreInventoryManagementSystem.bookstoremanagement.repository.AuthorRepository;
 import com.BookStoreInventoryManagementSystem.bookstoremanagement.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,11 +20,19 @@ public class BookServiceImpl implements BookService {
 	@Autowired
 	private BookRepository bookRepository;
 	@Autowired
+	private AuthorRepository authorRepository;
+	@Autowired
 	private BookConverter bookConverter;
 
 	@Override
 	public BookDto saveBook(BookDto bookDto) {
 		BookEntity bookEntity = bookRepository.save(bookConverter.convertBookDtoToEntity(bookDto));
+		for(AuthorEntity authorEntity: bookEntity.getAuthorEntitySet()) {
+			authorEntity.setBookEntity(bookEntity);
+			authorRepository.save(authorEntity);
+
+		}
+		
 		return bookConverter.convertBookEntityToDto(bookEntity);
 	}
 //
